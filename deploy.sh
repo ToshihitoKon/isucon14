01c9ee7da62ff81674e92aec5dcaa1d10a23df14
Kon Toshihito <toshihito.kon@gmail.com>
2024-12-08 12:13:56 +0900

    すma直

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+set -eux
 cd "$(dirname "$0")"
+rootdir=`pwd`
+
 # git fetch
 # git merge origin/main
 
+cd "${rootdir}/go"
+go build -o isuride ./...
+
+cd $rootdir
 # 設定ファイルをコピー
 sudo cp -r ./etc/nginx /etc/
 sudo cp -r ./etc/mysql /etc/
@@ -13,3 +20,5 @@ sudo systemctl restart mysql
 sudo systemctl restart nginx
 
 echo "デプロイ"
+~
+~
