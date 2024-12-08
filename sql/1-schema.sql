3e4b9bca737ce2c6ab7d08bbc98c0bb1dd1da2d7
hacusk <5301731+hacusk@users.noreply.github.com>
2024-12-08 12:13:49 +0900

    feat: 複合indexに

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -49,7 +49,7 @@ CREATE TABLE chair_locations
   created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '登録日時',
   PRIMARY KEY (id),
   INDEX idx_chair_id (chair_id),
-  INDEX idx_created_at (created_at),
+  INDEX idx_chair_id_created_at (chair_id, created_at),
   INDEX idx_latitude_longitude (latitude, longitude)
 )
   COMMENT = '椅子の現在位置情報テーブル';
