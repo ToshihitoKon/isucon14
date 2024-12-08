130d8d42f724aec1cb5490bbeb86b7f867cc59d1
hacusk <5301731+hacusk@users.noreply.github.com>
2024-12-08 11:38:32 +0900

    feat: latitude, longitude

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -48,7 +48,8 @@ CREATE TABLE chair_locations
   longitude  INTEGER     NOT NULL COMMENT '緯度',
   created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '登録日時',
   PRIMARY KEY (id),
-  INDEX idx_chair_id (chair_id)
+  INDEX idx_chair_id (chair_id),
+  INDEX idx_chair_id_latitude_longitude (chair_id, latitude, longitude)
 )
   COMMENT = '椅子の現在位置情報テーブル';
 
